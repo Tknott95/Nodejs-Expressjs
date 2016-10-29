01d832c0406183cf30ae80092348791ac5346a8b
tknott95 <tknott95@hotmail.com>
2016-10-29 14:03:46 -0600

    Bower Managed jqeury - BS

--- expressapp1/app.js
+++ expressapp1/app.js
@@ -6,6 +6,7 @@ var port = 8000;
 //static dir  for statics or imports 
  app.use(express.static('public'));  //tells public dir is static dir if resorces not found call the req
  app.use(express.static('src/views'));
+ app.use(express.static('bower_components')); //now everything in this file is available to index.hmtl etc
 
  app.get('/', function(req, res){    //what to do whn request comes in    ....  get will handle get rquest when comes in to '<link>'
     res.send("When other websites give you  yada yda yada ..");
