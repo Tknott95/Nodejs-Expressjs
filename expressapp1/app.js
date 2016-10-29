688e19684383b6d86e554ca5efb5cb003f023bc7
tknott95 <tknott95@hotmail.com>
2016-10-29 13:28:12 -0600

    Express simple get request

--- expressapp1/app.js
+++ expressapp1/app.js
@@ -3,6 +3,10 @@ var app = express();  //makes instance of express
 
 var port = 5280;
 
+ app.get('/', function(req, res){    //what to do whn request comes in    ....  get will handle get rquest when comes in to '<link>'
+    res.send("When other websites give you  yada yda yada ..");
+ });    
+
 app.listen(port, function(err){
     console.log('The server is running on port '+ port);
 })
