b389810fb8139e9c2e295d43bcb467047b303dfa
tknott95 <tknott95@hotmail.com>
2016-10-29 13:30:05 -0600

    Routing Check

--- expressapp1/app.js
+++ expressapp1/app.js
@@ -5,8 +5,12 @@ var port = 5280;
 
  app.get('/', function(req, res){    //what to do whn request comes in    ....  get will handle get rquest when comes in to '<link>'
     res.send("When other websites give you  yada yda yada ..");
- });    
+ });  
 
-app.listen(port, function(err){
+ app.get('/routing', function(req, res){
+     res.send('Aloha Routing');
+ })  
+
+ app.listen(port, function(err){
     console.log('The server is running on port '+ port);
-})
+ })
